25b1f4c7399e7ca00dba42490d7f998d1dabfb91
paypulse <sourcedev5@gmail.com>
2018-07-04 21:20:31 +0900

    20180704_InputManager

--- SpaceShooter/Assets/02.Scripts/PlayerCtrl.cs
+++ SpaceShooter/Assets/02.Scripts/PlayerCtrl.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour {
 
+	private float h = 0.0f;
+	private float v = 0.0f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +14,11 @@ public class PlayerCtrl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		h = Input.GetAxis ("Horizontal");
+		v = Input.GetAxis ("Vertical");
+
+		Debug.Log ("h=" + h.ToString());
+		Debug.Log ("v=" + v.ToString());
 		
 	}
 }
